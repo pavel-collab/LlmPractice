e8aa232edb30293ba35c7ee276a724165ae8334a
Filippenko Pavel <filippenko.ps@phystech.edu>
2024-12-02 01:22:25 +0300

    work with Tweets

---
+++ Tweets/data.py
@@ -0,0 +1,52 @@
+import torch
+from torch.utils.data import Dataset
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader, random_split
+from transformers import AutoTokenizer
+
+class TextDataset(Dataset):
+    def __init__(self, texts, labels, tokenizer_name, max_length):
+        self.texts = texts
+        self.labels = labels
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        text = self.texts[idx]
+        label = self.labels[idx]
+
+        # Токенизация текста
+        encoding = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=self.max_length,
+            return_token_type_ids=False,
+            padding='max_length',
+            truncation=True,
+            return_attention_mask=True,
+            return_tensors='pt',
+        )
+        
+        return {
+            'input_ids': encoding['input_ids'].flatten(),
+            'attention_mask': encoding['attention_mask'].flatten(),
+            'labels': torch.tensor(label, dtype=torch.long)
+        }
+    
+class TextDataModule(pl.LightningDataModule):
+    def __init__(self, dataset, batch_size=32):
+        super().__init__()
+        self.dataset = dataset
+        self.batch_size = batch_size
+
+    def setup(self, stage=None):
+        self.train_dataset, self.val_dataset = random_split(self.dataset, [int(len(self.dataset)*0.8), len(self.dataset) - int(len(self.dataset)*0.8)])
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size)
